4d55bb4f7f1cf19f84358898f2619c618bba8185
SSung023 <50323157+SSung023@users.noreply.github.com>
2023-11-13 21:41:34 +0900

    refactor: change enum order
    - EventType enum에서 PRESENT의 순서를 제일 뒤로 변경

--- src/main/java/christmas/constants/EventType.java
+++ src/main/java/christmas/constants/EventType.java
@@ -1,11 +1,11 @@
 package christmas.constants;
 
 public enum EventType {
-    PRESENT("증정 이벤트"),
     CHRISTMAS("크리스마스 디데이 할인"),
     WEEKDAY("평일 할인"),
     WEEKEND("주말 할인"),
-    SPECIAL("특별 할인");
+    SPECIAL("특별 할인"),
+    PRESENT("증정 이벤트");
 
     private final String description;
 
